0ecc79b1caa180ea8f540373a3aa756c09e64f38
Roel Antonisse <roel.antonisse@gmail.com>
2023-08-12 13:41:35 +0200

    Remove unused rollup-plugin-copy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 import requireTransform from 'vite-plugin-require-transform';
 
-import copy from 'rollup-plugin-copy';
 import mergeJson from './rollup/rollup-plugin-merge-json/index.js';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
@@ -34,71 +33,6 @@ export default defineConfig({
       verbose: false,
       watch: true,
       wrapWithPath: true
-    }),
-
-    copy({
-      targets: [
-        {
-          // Styles
-          src: 'src/static/css/**/*.css',
-          dest: `build/bundles`
-        },
-        {
-          // Images
-          src: 'src/static/**/*.{svg,png,jpeg,jpg}',
-          dest: `build/images`
-        },
-        {
-          // index.html
-          src: 'index.html',
-          dest: 'build',
-          transform: (contents) => {
-            let contentsString = contents.toString();
-            const buildDate = new Date();
-            const months = [
-              'January',
-              'February',
-              'March',
-              'April',
-              'May',
-              'June',
-              'July',
-              'August',
-              'September',
-              'October',
-              'November',
-              'December'
-            ];
-
-            const replacement = {
-              __APP_BUILD_DATE__: `${buildDate.getDate()} ${
-                months[buildDate.getMonth()]
-              } ${buildDate.getFullYear()}`,
-              __APP_BUILD_YEAR__: buildDate.getFullYear(),
-              __APP_VERSION__: production ? pkg.version : 'DEVELOPMENT',
-              __BASEPATH__: BASEPATH,
-              __TITLE__: pkg.name
-            };
-
-            let replaceRegexp;
-
-            // e.g. "{__TITLE__}" => "WCAG-EM-Report-Tool"
-            for (var key in replacement) {
-              replaceRegexp = new RegExp(`\\{${key}\\}`, 'g');
-
-              if (Object.prototype.hasOwnProperty.call(replacement, key)) {
-                contentsString = contentsString.replace(
-                  replaceRegexp,
-                  replacement[key]
-                );
-              }
-            }
-
-            return contentsString;
-          }
-        }
-      ],
-      verbose: false
     }),
 
     replace({
