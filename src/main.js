9bfbb4b6e94f38853fcc7ce855cc0e16845e25d9
Roel Antonisse <roel.antonisse@gmail.com>
2023-08-17 11:55:38 +0200

    Change to dynamic importing
    add docs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import { internationalize } from '@app/scripts/i18n.js';
 import languages from '@app/locales/index.json';
 import App from '@app/components/App.svelte';
 
+/**
+ * @type {string[]}
+ * https://www.rfc-editor.org/rfc/rfc5646.txt lang codes
+ */
 const locales = languages.map((language) => language.lang);
 
 internationalize(locales).then(() => {
