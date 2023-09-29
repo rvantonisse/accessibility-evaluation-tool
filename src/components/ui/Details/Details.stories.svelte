40b396a55f002751636e199da596dcf9cee4bbde
Roel Antonisse <roel.antonisse@gmail.com>
2023-09-29 16:11:03 +0200

    Add Details Component with default story

---
+++ src/components/ui/Details/Details.stories.svelte
@@ -0,0 +1,15 @@
+<Meta 
+    title="UI/Details"
+/>
+
+<Story name="Default">
+    <Details>
+        <p>Some disclosed details here.</p>
+    </Details>
+</Story>
+
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+  import Details from './Details.svelte';
+</script>
---
+++ src/components/ui/Details/Details.svelte
@@ -0,0 +1,57 @@
+<!-- 
+@component
+
+# Details
+
+Discloses additional information.
+
+ -->
+
+<details class="Details" bind:open="{open}">
+  <summary role="button" class="Button small">{summary}</summary>
+
+  <div class="Details_contents">
+    <slot />
+  </div>
+</details>
+
+<style>
+  @import '@app/components/ui/Button/styles.css';
+
+  summary {
+    position: relative;
+    display: inline-flex;
+    list-style: none;
+  }
+
+  summary::-webkit-details-marker {
+    display: none;
+  }
+
+  summary::after {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    
+    /*
+     * @todo content alt text is not supported for webkit 
+     * replace with html or css url image
+    */
+    content: "+";
+    content: "+" / "";
+
+    width: 1rem;
+    height: 1rem;
+  }
+
+  [open] > summary::after {
+    content: "–";
+    content: "–" / "";
+  }
+
+</style>
+
+<script>
+  export let open = 'true';
+  export let summary = 'details';
+</script>
